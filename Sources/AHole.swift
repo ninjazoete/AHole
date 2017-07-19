8cab28c764dfe4a9d44f043b1b69935b77d32b94
Andrzej Spiess <andrzej.spiess@gmail.com>
2017-07-19 21:26:01 +0200

    Removed conditional fatalError

--- Sources/AHole.swift
+++ Sources/AHole.swift
@@ -13,35 +13,6 @@ public protocol AHoleType {
 }
 
 public func _hole_<A: AHoleType>(file: StaticString = #file, line: UInt = #line) -> A {
-    _holeConditional(file: file, line: line)
+    print("AHOLE:: Using a hole at line \(line) in file \(file)")
     return A.holed()
-}
-
-// MARK: Private Utility
-
-private func _runsOnSimulator() -> Bool {
-    #if (arch(i386) || arch(x86_64)) && (os(iOS) || os(watchOS) || os(tvOS))
-        return true
-    #else
-        return false
-    #endif
-}
-
-private func _holeConditional(file: StaticString, line: UInt) {
-    if _runsOnSimulator() {
-        
-        /*
-         Assuming simulator is a debug build, print each hole's location.
-        */
-        
-        print("AHOLE:: Using a hole at line \(line) in file \(file)")
-    } else {
-        
-        /*
-         When used on real device there is a big chance it is a production build
-         so we don't allow holes.
-        */
-        
-        fatalError("AHOLE:: A hole is used at line \(line) in file \(file)")
-    }
 }
